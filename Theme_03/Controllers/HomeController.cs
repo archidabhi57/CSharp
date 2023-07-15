c203b115d11bcc430b58ab705778de67e7fffcd1
archidabhi57 <109404841+archidabhi57@users.noreply.github.com>
2023-07-15 19:24:31 +0530

    .

---
+++ Theme_03/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Resume()
+        {
+            return View();
+        }
+
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
